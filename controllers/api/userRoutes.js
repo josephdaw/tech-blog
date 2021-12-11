c820b3300df9fab6f6561e77acfd7d6e2d83d410
josephdaw <josephdaw@icloud.com>
2021-12-11 18:42:19 +1030

    adding update user information functionality

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,24 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+
+// create new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// update existing user information
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,7 +79,6 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Post }],
     });
 
     const user = userData.get({ plain: true });
